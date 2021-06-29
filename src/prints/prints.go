b3141a83e671e01232e63e8022002d7a1261de3a
k0rup710n <61102077+k0rup710n@users.noreply.github.com>
2021-06-29 16:21:19 -0400

    Add files via upload

---
+++ src/prints/prints.go
@@ -0,0 +1,39 @@
+package prints
+
+import "github.com/fatih/color"
+
+func PrintBanner() {
+	color.New(color.FgGreen, color.Bold).Println("  ________      _________________________  ______________")
+	color.New(color.FgGreen, color.Bold).Println(" /  _____/  ____\\_   _____/\\______   \\   \\/  /\\__    ___/")
+	color.New(color.FgGreen, color.Bold).Println("/   \\  ___ /  _ \\|    __)   |    |  _/\\     /   |    |   ")
+	color.New(color.FgGreen, color.Bold).Println("\\    \\_\\  (  <_> )     \\    |    |   \\/     \\   |    |   ")
+	color.New(color.FgGreen, color.Bold).Println(" \\______  /\\____/\\___  /    |______  /___/\\  \\  |____|   ")
+	color.New(color.FgGreen, color.Bold).Println("        \\/           \\/            \\/      \\_/           ")
+	color.New(color.FgGreen, color.Bold).Println("\t\t~ Go Facebook Extractor v 1.0")
+	color.New(color.FgGreen, color.Bold).Print("\t\t\tBy")
+	color.New(color.FgYellow, color.Bold).Print(" Gremlin\n\n")
+}
+
+func PrintPlus() {
+	color.New(color.FgWhite, color.Bold).Printf("[")
+	color.New(color.FgGreen, color.Bold).Printf("+")
+	color.New(color.FgWhite, color.Bold).Printf("] ")
+}
+
+func PrintExclamation() {
+	color.New(color.FgWhite, color.Bold).Printf("[")
+	color.New(color.FgRed, color.Bold).Printf("!")
+	color.New(color.FgWhite, color.Bold).Printf("] ")
+}
+
+func PrintInterrogation() {
+	color.New(color.FgWhite, color.Bold).Printf("[")
+	color.New(color.FgYellow, color.Bold).Printf("?")
+	color.New(color.FgWhite, color.Bold).Printf("] ")
+}
+
+func PrintToken() {
+	color.New(color.FgWhite, color.Bold).Printf("[")
+	color.New(color.FgYellow, color.Bold).Printf("TOKEN")
+	color.New(color.FgWhite, color.Bold).Printf("] ")
+}
